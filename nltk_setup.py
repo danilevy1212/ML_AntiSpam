446ab98bb1be0b492c665dc6d0b7d181c0dca25d
danilevy1212 <danilevy1212@gmail.com>
2019-06-11 20:52:54 +0200

    Simple script to download all the nltk dependencies

---
+++ nltk_setup.py
@@ -0,0 +1,8 @@
+import nltk
+
+if __name__ == "__main__":
+    nltk.download('punkt')
+    nltk.download('stopwords')
+    nltk.download('wordnet')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('words')
